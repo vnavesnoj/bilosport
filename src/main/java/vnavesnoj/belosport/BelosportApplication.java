db7b27163174295685e2d1927a14201c9e82bb3f
vnavesnoj <vnavesnoj@gmail.com>
2023-08-22 10:46:30 +0300

    First commit

---
+++ src/main/java/vnavesnoj/belosport/BelosportApplication.java
@@ -0,0 +1,13 @@
+package vnavesnoj.belosport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelosportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelosportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vnavesnoj/belosport/BelosportApplicationTests.java
@@ -0,0 +1,13 @@
+package vnavesnoj.belosport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelosportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
