aff0d51eef5a66f395d24054a37c6f27b4a0601f
vnavesnoj <vnavesnoj@gmail.com>
2023-08-26 21:35:36 +0300

    Add CoachAthleteRepository

---
+++ src/main/java/vnavesnoj/spring/database/repository/CoachAthleteRepository.java
@@ -0,0 +1,18 @@
+package vnavesnoj.spring.database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vnavesnoj.spring.database.entity.CoachAthlete;
+import vnavesnoj.spring.database.entity.User;
+
+import java.util.List;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+public interface CoachAthleteRepository extends JpaRepository<CoachAthlete, Long> {
+
+    List<CoachAthlete> findAllByCoachId(Long id);
+
+    List<CoachAthlete> findAllByAthlete(User id);
+}
---
+++ src/test/java/vnavesnoj/spring/integration/repository/CoachAthleteRepositoryTest.java
@@ -0,0 +1,23 @@
+package vnavesnoj.spring.integration.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import vnavesnoj.spring.database.repository.CoachAthleteRepository;
+import vnavesnoj.spring.integration.annotation.IT;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+@IT
+@RequiredArgsConstructor
+class CoachAthleteRepositoryTest {
+
+    private final CoachAthleteRepository coachAthleteRepository;
+
+    @Test
+    void test() {
+        final var athletes = coachAthleteRepository.findAllByCoachId(1L);
+        System.out.println();
+    }
+}
