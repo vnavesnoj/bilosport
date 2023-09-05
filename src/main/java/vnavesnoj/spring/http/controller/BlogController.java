33be180cabbba0abbd13d1db4f3024b96c6fa774
vnavesnoj <vnavesnoj@gmail.com>
2023-09-05 09:00:25 +0300

    Add findById() in BlogController

--- src/main/java/vnavesnoj/spring/http/controller/BlogController.java
+++ src/main/java/vnavesnoj/spring/http/controller/BlogController.java
@@ -1,10 +1,15 @@
 package vnavesnoj.spring.http.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import vnavesnoj.spring.dto.BlogCreateEditDto;
 import vnavesnoj.spring.dto.BlogInfoReadDto;
 import vnavesnoj.spring.service.BlogService;
 
@@ -28,8 +33,25 @@ public class BlogController {
         return "blog/blogs";
     }
 
-    @GetMapping("news/add")
+    @GetMapping("/add")
     public String add(Model model) {
         return "blog/blog-add";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable Integer id) {
+        blogService.findById(id).ifPresentOrElse(
+                blog -> model.addAttribute("blog", blog),
+                () -> {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+                }
+        );
+        return "blog/blog";
+    }
+
+    @PostMapping("/add")
+    public String post(Model model, BlogCreateEditDto blogDto) {
+        final var blog = blogService.create(blogDto);
+        return "redirect:/news/" + blog.getId();
     }
 }
